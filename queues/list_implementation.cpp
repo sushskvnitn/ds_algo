594837c893290b9e752aeb6b0d48ee4546978b40
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-01-15 12:07:50 +0530

    list implementation of queues

---
+++ queues/list_implementation.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+// creating linked list
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+class queues {
+    node *front ;
+    node * back;
+    public:
+    queue(){
+        front=NULL;
+        back=NULL;
+    }
+
+    void push( int x){
+        node * n =new node(x);
+        if(front ==NULL){
+            back =n;
+            front =n;
+            return;
+
+        }
+        back->next =n;
+        back =n;
+    }
+
+    void pop(){
+        if(front ==NULL){
+            cout<<"queue underflow"<<endl;
+            return;
+        }
+        node* todelete =front ;
+        front =front->next;
+        delete todelete;
+    }
+    int peek(){
+        if(front ==NULL){
+            cout<< " no element in queue"<<endl;
+            return -1;
+        }
+        return front->data;
+    }
+    bool empty(){
+        if(front ==NULL){
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    queues q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.empty()<<endl;
+    return 0;
+}
