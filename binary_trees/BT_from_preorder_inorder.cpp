37989376b7925c87e88755efd130910424fdb510
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-01-21 23:26:43 +0530

    building tree using inorder postorder and preorder

---
+++ binary_trees/BT_from_preorder_inorder.cpp
@@ -0,0 +1,71 @@
+// algorithm:
+// 1> pick element from preorder and create a Node
+// 2> increment pre order index 
+// 3> search for picked element position in inorder
+// 4> call to build left subtreee from inorder 0 to pos-1
+// 4> call to build right subtreee from inorder pos to n
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+struct Node{
+    int data;
+    Node* left,*right;
+    Node(int val){
+        data = val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+// search in inorder 
+int search(int inorder[],int start,int end,int curr){
+ //start and end are of inorder and curr is of prev order 
+  for(int i =start;i<=end;i++){
+      if(inorder[i]==curr){
+          return i;
+      }
+  }
+    return -1;
+}
+
+//building a tree 
+Node *buildtree(int preorder[],int inorder[],int start,int end){
+    static int idx =0;
+    //base condition 
+    if(start > end){
+        return NULL;
+    }
+
+    int curr  = preorder[idx];
+    idx++;
+    Node* node = new Node(curr);
+    //for 1 element tree
+    if(start ==end){
+        return node;
+    }
+    //pos - for position in inorder 
+    int pos = search(inorder,start,end,curr);
+    node->left= buildtree(preorder,inorder,start,pos-1);
+    node->right= buildtree(preorder,inorder,pos+1,end);
+    return node;
+}
+
+
+//print inorder 
+void prinorder(Node * root){
+    if(root==NULL){
+        return;
+    }
+    prinorder(root->left);
+    cout<<root->data<<" ";
+    prinorder(root->right);
+}
+int main(){ 
+
+int preorder[]={1,2,4,3,5};
+int inorder[]={4,2,1,5,3};
+Node *root = buildtree(preorder,inorder,0,4);
+prinorder(root);
+ return 0;
+}
