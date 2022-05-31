4fb58ef41bbbebadf67dbde9df8e4adf911ce9a0
Sushant Kogurwar <88105162+sushskvnitn@users.noreply.github.com>
2022-05-31 10:55:51 +0530

    Update adjacencymatrix.cpp

--- graphs/adjacencymatrix.cpp
+++ graphs/adjacencymatrix.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+
 int main()
 {
-      int n,m;//n-> no of nodes   ,m -> no of edges
-      cout<<"Enter the number of nodes and edges: ";
-      cin >>n>>m;
-      vector<vector<int> > adj(n+1,vector<int>(n+1,0));
-      //for undirected graph
-      for (int i=0;i<m;i++){
-            int x,y;
-            cin >>x>>y;
-            adj[x][y]=1;
-            adj[y][x]=1;
-      }
-      cout<<"----------------adjacency matrix----------------"<<endl;
-      for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                  cout<<adj[i][j]<<" ";
-            }
-            cout<<endl;
-      }
-
-      return 0;
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>adjm(n+1,vector<int>(n+1,0));
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin>>x>>y;
+        adjm[x][y]=1;
+        adjm[y][x]=1;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+            cout<<adjm[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
