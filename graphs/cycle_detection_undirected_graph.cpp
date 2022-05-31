c9ad7674e7b6457a39e750bcce69dba2d00d57dc
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-05-31 13:21:09 +0530

    crt code

--- graphs/cycle_detection_undirected_graph.cpp
+++ graphs/cycle_detection_undirected_graph.cpp
@@ -5,13 +5,15 @@ using namespace std;
 //and returns false if cycle detected
 bool iscyclic(int src,vector<vector<int> > &adj,vector<bool> &visited,int parent){
       visited[src]=true;
-      for(int i=0;i<adj[src].size();i++){
-            if(i!=parent){
-               if(visited[i])
-                  return true;
-               if(!visited[i] && iscyclic(i,adj,visited,src) ){
-                      return true;
-               }
+      vector<int> ::iterator it;
+      for(it=adj[src].begin();it!=adj[src].end();it++){
+            if(*it!=parent){
+                  if (visited[*it]){
+                        return true;
+                  } 
+                  if(!visited[*it] &&iscyclic(*it,adj,visited,src)){ 
+                        return true;
+                  } 
             }
       }
       return false;
@@ -24,7 +26,6 @@ int main()
       cout << "enter the number of nodes  and edges " << endl;
       cin >> n >> m;
       vector<vector<int> > adj(n);
-
       for (int i = 0; i < m; i++)
       {
             int x, y;
